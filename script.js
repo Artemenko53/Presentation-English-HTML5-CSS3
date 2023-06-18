5bd48db93800e463652de8bd220c9cccfb6c0ffc
Artemenko53 <127668595+Artemenko53@users.noreply.github.com>
2023-06-18 18:25:58 +0300

    Site dublicated
    Site was downloaded from replit as zip and was dublicated to GitHub

---
+++ script.js
@@ -0,0 +1,18 @@
+function getFullscreenElement() {
+  return document.fullscreenElement;
+  document.webkitFullscreenElement;
+  document.mozFullscreenElement;
+  document.msFullscreenElement;
+}
+
+function toggleFullscreen() {
+  if (getFullscreenElement()) {
+    document.exitFullscreen();
+  } else {
+    document.documentElement.requestFullscreen().catch(console.log);
+  }
+}
+
+document.addEventListener("dblclick", () => {
+  toggleFullscreen();
+});
